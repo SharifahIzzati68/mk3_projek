11d2a96ce6b307ecb3707a873ed37a8d8e585e04
anis2508 <18DDT21F1039@student.ptss.edu.my>
2023-09-18 11:07:46 +0800

    anis

--- admin/kemaskini.php
+++ admin/kemaskini.php
@@ -4,6 +4,34 @@ require '../include/conn.php';
 $idwarden = $_POST['idwarden'];
 $namawarden = $_POST['namawarden'];
 $nokpwarden= $_POST['nokpwarden'];
+// Check if the nokpwarden (warden's identification number) already exists
+$check_sql = "SELECT COUNT(*) AS count FROM warden WHERE nokpwarden = '$nokpwarden'";
+$check_result = $conn->query($check_sql);
+$row = $check_result->fetch_assoc();
+if ($row['count'] > 0) {
+    // Redirect back to the warden registration page with an error message
+    header('location: index.php?menu=warden&error=exists');
+    exit;
+}
+// Check if the provided 'nokppelajar' is not a warden's 'No.KP warden'
+$pelajar_check_sql = "SELECT COUNT(*) AS count FROM pelajar WHERE nokppelajar = '$nokpwarden'";
+$pelajar_check_result = $conn->query($pelajar_check_sql);
+
+if (!$pelajar_check_result) {
+    // Handle the query error
+    echo "Error: " . $conn->error;
+    $conn->close();
+    exit;
+}
+
+$pelajar_row = $pelajar_check_result->fetch_assoc();
+
+if ($pelajar_row['count'] > 0) {
+    // Redirect back to the warden registration page with an error message
+    header('location: index.php?menu=warden&error=pelajar');
+    exit;
+}
+
 $sql = "UPDATE warden
 SET namawarden = '$namawarden', nokpwarden = '$nokpwarden'
 WHERE idwarden = $idwarden";
--- warden/kemaskini.php
+++ warden/kemaskini.php
@@ -7,6 +7,25 @@ $namapelajar = $_POST['namapelajar'];
 $nokppelajar= $_POST['nokppelajar'];
 $katas= $_POST['nokppelajar'];
 
+// Check if the nokppelajar already exists in the 'pelajar' table
+$check_sql = "SELECT COUNT(*) AS count FROM pelajar WHERE nokppelajar = '$nokppelajar'";
+$check_result = $conn->query($check_sql);
+
+if (!$check_result) {
+    // Handle the query error
+    echo "Error: " . $conn->error;
+    $conn->close();
+    exit;
+}
+
+$row = $check_result->fetch_assoc();
+
+if ($row['count'] > 0) {
+    // Redirect back to the warden registration page with an error message
+    header('location: index.php?menu=Student&error=exists');
+    exit;
+}
+
 // Check if the provided 'nokppelajar' is not a warden's 'No.KP warden'
 $warden_check_sql = "SELECT COUNT(*) AS count FROM warden WHERE nokpwarden = '$nokppelajar'";
 $warden_check_result = $conn->query($warden_check_sql);
