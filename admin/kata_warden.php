73c01dae9ed3369fb26587c5861991150c6ced3e
qurratulaini3010 <missquratulaini@gmail.com>
2023-11-14 16:56:48 +0800

    qurratul

--- admin/kata_warden.php
+++ admin/kata_warden.php
@@ -23,18 +23,18 @@ if (isset($_POST['idadmin'])) {
                 $hash_password = password_hash($new_password, PASSWORD_BCRYPT);
                 $sql = "UPDATE admin SET kata = '$hash_password' WHERE idadmin = $idadmin";
                 if ($conn->query($sql)) {
-                    echo "Password updated successfully!";
+                    echo "Kata laluan berjaya dikemaskini!";
                 } else {
-                    echo "Error updating password: " . $conn->error;
+                    echo "Ralat semasa mengemaskini kata laluan: " . $conn->error;
                 }
             } else {
-                echo "New password and confirm new password do not match.";
+                echo "Kata laluan baru dan pengesahan kata laluan baru tidak sepadan.";
             }
         } else {
-            echo "Current password is incorrect.";
+            echo "Kata laluan semasa tidak betul.";
         }
     } else {
-        echo "Error fetching current password: " . $conn->error;
+        echo "Ralat mendapatkan kata laluan semasa: " . $conn->error;
     }
 }
 
@@ -51,7 +51,7 @@ if (!$result) {
 $row = $result->fetch_object();
 
 if (!$row) {
-    echo "No data found for this warden.";
+    echo "Tiada data warden yang dijumpai .";
     exit;
 }
 
@@ -71,22 +71,22 @@ $idadmin = $row->idadmin;
 <form action="index.php?menu=kata_warden" method="post">
     <input type="hidden" name="idadmin" value="<?php echo $row->idadmin; ?>">
     <fieldset>
-        <legend><h1>Change password</h1></legend>
+        <legend><h1>Tukar Kata Laluan</h1></legend>
         <table>
             <tr>
-                <th class="th">Current Password</th>
+                <th class="th">Kata Laluan Semasa</th>
                 <td><label>
                         <input type="password" name="current_password" required>
                     </label></td>
             </tr>
             <tr>
-                <th class="th">New Password</th>
+                <th class="th">Kata Laluan Baru</th>
                 <td><label>
                         <input type="password" name="new_password" required>
                     </label></td>
             </tr>
             <tr>
-                <th>Confirm New Password</th>
+                <th>Pengesahan Kata Laluan Baru</th>
                 <td><label>
                         <input type="password" name="confirm_password" required>
                     </label></td>
